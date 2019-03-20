0d8525bcf8eecc3b7105ea7d006c9509dfeca7d7
yusupure <44675048+yusupure@users.noreply.github.com>
2019-03-20 23:05:18 +0800

    Update mnist_test.py

--- 手写识别模块化/mnist_test.py
+++ 手写识别模块化/mnist_test.py
@@ -1,46 +1,56 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar  4 22:20:31 2019
+Created on Wed Mar 20 21:40:45 2019
 
 @author: Administrator
 """
 
 import tensorflow as tf
-from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
-import mnist_backward_00
-import mnist_farward_00
+import mnist_backward
+import mnist_farward
 import time
-
-def test(mnist):
+#from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
+import mnist_tfrecord
+batch_size=10000
+def test():
     with tf.Graph().as_default() as g:
-        x=tf.placeholder(tf.float32,[None,mnist_farward_00.input_mode])
-        y=tf.placeholder(tf.float32,[None,mnist_farward_00.output_mode])
-        pd=mnist_farward_00.farward(x,None)
+        x=tf.placeholder(tf.float32,[None,mnist_farward.input_mode])
+        y=tf.placeholder(tf.float32,[None,mnist_farward.output_mode])
+        pd=mnist_farward.farward(x,None)
+        #pd=tf.argmax(y,1)
         
-        #滑动平均
-        ema=tf.train.ExponentialMovingAverage(mnist_backward_00.moving_reta)
-        ema_restort=ema.variables_to_restore()
-        saver=tf.train.Saver(ema_restort)
-        #转换布尔值为小数
+        #滑动搞平均
+        ema=tf.train.ExponentialMovingAverage(mnist_backward.moving_rate)
+        ema_restord=ema.variables_to_restore()
+        saver=tf.train.Saver(ema_restord)
         cp=tf.equal(tf.argmax(pd,1),tf.argmax(y,1))
         acc=tf.reduce_mean(tf.cast(cp,tf.float32))
+        img_batch,labels_batch=mnist_tfrecord.Tfrecorde_load(batch_size,isTrain=False)
         while True:
-            with tf.Session()as sess:
-                ckpt=tf.train.get_checkpoint_state(mnist_backward_00.model_save_path)
+            with tf.Session() as sess:
+                ckpt=tf.train.get_checkpoint_state(mnist_backward.model_save_path)
                 if ckpt and ckpt.model_checkpoint_path:
                     saver.restore(sess,ckpt.model_checkpoint_path)
+                    
+                    coord=tf.train.Coordinator()
+                    theads=tf.train.start_queue_runners(sess=sess,coord=coord)
+                    
                     global_step=ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
-                    accs=sess.run(acc,feed_dict={x:mnist.test.images,y:mnist.test.labels})
+                    #accs=sess.run(acc,feed_dict={x:mnist.test.images,y:mnist.test.labels})
+                    xs,ys=sess.run([img_batch,labels_batch])
+                    accs=sess.run(acc,feed_dict={x:xs,y:ys})
                     print(global_step,accs)
+                    coord.request_stop()
+                    coord.join(theads)
+                                 
                 else:
-                        print('No,model')
+                    print('没有训练包')
+                    return
             time.sleep(5)
-        
-def main():
-    mnist=read_data_sets('mnist_data',one_hot=True)
-    test(mnist)
 
+def main():
+    #mnist=read_data_sets('mnist_data',one_hot=True)
+    #test(mnist)
+    test()
 if __name__=='__main__':
-    main()                
-                
-        
+    main()
