a921387e544cbe21709f30ccec1692fd14f5fbe3
yusupure <44675048+yusupure@users.noreply.github.com>
2019-03-20 23:04:09 +0800

    Update mnist_app.py

--- 手写识别模块化/mnist_app.py
+++ 手写识别模块化/mnist_app.py
@@ -1,77 +1,58 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar  5 22:08:05 2019
+Created on Wed Mar 20 21:51:06 2019
 
 @author: Administrator
 """
+
+import tensorflow as tf
+import mnist_backward
+import mnist_farward
 import numpy as np
 from PIL import Image
-import mnist_backward_00
-import mnist_farward_00
-import tensorflow as tf
 
-def restore_model(testPicArr):
-
-    with tf.Graph().as_default() as tg:
-        
-        x = tf.placeholder(tf.float32,[None,mnist_farward_00.input_mode])
-        y = mnist_farward_00.farward(x,None)
-        preValue = tf.argmax(y,1)
-        # 加载滑动平均模型
-        variable_averages = tf.train.ExponentialMovingAverage(mnist_backward_00.moving_reta)
-        variables_to_restore = variable_averages.variables_to_restore()
-        saver = tf.train.Saver(variables_to_restore)
-
+def image_search(image_data):
+    with tf.Graph().as_default()as tg:
+        x=tf.placeholder(tf.float32,[None,mnist_farward.input_mode])
+        pd=mnist_farward.farward(x,None)
+        loss=tf.argmax(pd,1)
+        #滑动平均
+        vt=tf.train.ExponentialMovingAverage(mnist_backward.moving_rate)
+        vtr=vt.variables_to_restore()
+        saver=tf.train.Saver(vtr)
         with tf.Session() as sess:
-            
-            ckpt = tf.train.get_checkpoint_state(mnist_backward_00.model_save_path)
+            ckpt=tf.train.get_checkpoint_state(mnist_backward.model_save_path)
             if ckpt and ckpt.model_checkpoint_path:
-                # 恢复当前会话,将ckpt中的值赋值给w和b
                 saver.restore(sess,ckpt.model_checkpoint_path)
-                # 执行图计算
-                preValue = sess.run(preValue,feed_dict={x:testPicArr})
-                return preValue
-            else:
-                print("No checkpoint file found")
-                return -1
-# 图片预处理函数
-def pre_pic(picName):
-    # 先打开传入的原始图片
-    img = Image.open(picName)
-    # 使用消除锯齿的方法resize图片
-    reIm = img.resize((28,28),Image.ANTIALIAS)
-    # 变成灰度图，转换成矩阵
-    im_arr = np.array(reIm.convert("L"))
-    threshold = 50#对图像进行二值化处理，设置合理的阈值，可以过滤掉噪声，让他只有纯白色的点和纯黑色点
+                loss_data=sess.run(loss,feed_dict={x:image_data})
+                return loss_data
+
+def image_load(image_op):
+    image_open=Image.open(image_op)
+    img_data=image_open.resize((28,28),Image.ANTIALIAS)
+    img=np.array(img_data.convert('L'))
+    thod=50
     for i in range(28):
         for j in range(28):
-            im_arr[i][j] = 255-im_arr[i][j]
-            if (im_arr[i][j]<threshold):
-                im_arr[i][j] = 0
+            img[i][j]=255-img[i][j]
+            if (img[i][j]<thod):
+                img[i][j]=0
             else:
-                im_arr[i][j] = 255
-    # 将图像矩阵拉成1行784列，并将值变成浮点型（像素要求的仕0-1的浮点型输入）
-    nm_arr = im_arr.reshape([1,784])
-    nm_arr = nm_arr.astype(np.float32)
-    img_ready = np.multiply(nm_arr,1.0/255.0)
-
-    return img_ready
-
-def application():
-    # input函数可以从控制台接受数字
-    testNum = int(input("input the number of test images:"))
-    # 使用循环来历遍需要测试的图片才结束
-    for i in range(testNum):
-        # input可以实现从控制台接收字符格式,图片存储路径
-        testPic = input("the path of test picture:")
-        # 将图片路径传入图像预处理函数中
-        testPicArr = pre_pic(testPic)
-        # 将处理后的结果输入到预测函数最后返回预测结果
-        preValue = restore_model(testPicArr)
-        print("The prediction number is :",preValue)
-
+                img[i][j]=255
+    nm_arr=img.reshape([1,784])
+    nm_arr.astype(np.float32)
+    image_readly=np.multiply(nm_arr,1./255.)
+    return image_readly
+    
+def apploction():
+    image_index=int(input('输入测试图片数量：'))
+    for i in range(image_index):
+        image_op=input('图片地址')
+        image_data=image_load(image_op)
+        image_pp=image_search(image_data)
+        print(image_pp)
+    
 def main():
-    application()
-
-if __name__ == "__main__":
+    apploction()
+if __name__=='__main__':
     main()
